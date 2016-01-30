e96ac46c3fc5054ff6ef9066bb2fa228ad4c9199
Yann Vaillant <yann@vaillant.im>
2016-01-30 13:47:10 +0100

    WIP: Basic ui with working GIF/IMG

--- share/imgur-viewer/main.qml
+++ share/imgur-viewer/main.qml
@@ -1,28 +1,16 @@
 import QtQuick 2.4
-import Ubuntu.Components 1.2
-
-/*!
-    \brief MainView with a Label and Button elements.
-*/
+import Ubuntu.Components 1.3
 
 MainView {
-    // objectName for functional testing purposes (autopilot-qt5)
     objectName: "mainView"
-
-    // Note! applicationName needs to match the "name" field of the click manifest
     applicationName: "imgur-viewer.vayan"
-
-    /*
-     This property enables the application to change orientation
-     when the device is rotated. The default is false.
-    */
-    //automaticOrientation: true
+    automaticOrientation: true
 
     width: units.gu(100)
     height: units.gu(75)
 
     Page {
-        title: i18n.tr("Simple")
+        title: i18n.tr("Simple Imgur App")
 
         Column {
             spacing: units.gu(1)
@@ -30,21 +18,21 @@ MainView {
                 margins: units.gu(2)
                 fill: parent
             }
-
-            Label {
-                id: label
-                objectName: "label"
-
-                text: ctrl.message
-            }
-
             Button {
                 objectName: "button"
                 width: parent.width
+                text: i18n.tr("Next")
+                onClicked: ctrl.next()
+            }
 
-                text: i18n.tr("Tap me!!!")
-
-                onClicked: ctrl.hello()
+            AnimatedImage {
+              width: parent.width
+              height: parent.height
+              fillMode: Image.PreserveAspectFit
+              source: ctrl.source
+              playing: true
+              paused: false
+              onStatusChanged: playing = true
             }
         }
     }
--- src/imgur-viewer/main.go
+++ src/imgur-viewer/main.go
@@ -1,48 +1,74 @@
 package main
 
 import (
-        "gopkg.in/qml.v1"
-        "log"
+	"encoding/json"
+	"gopkg.in/qml.v1"
+	"log"
+	"net/http"
 )
 
+type response struct {
+	Data []image `json:"data"`
+}
+
+type image struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Link        string `json:"link"`
+}
+
+type control struct {
+	Root   qml.Object
+	K      int
+	Images []image
+	Source string
+}
+
 func main() {
-        err := qml.Run(run)
-        if (err != nil) {
-                log.Fatal(err)
-        }
+	err := qml.Run(run)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func run() error {
-        engine := qml.NewEngine()
-        component, err := engine.LoadFile("share/imgur-viewer/main.qml")
-        if err != nil {
-                return err
-        }
+	engine := qml.NewEngine()
+	component, err := engine.LoadFile("share/imgur-viewer/main.qml")
+	if err != nil {
+		return err
+	}
 
-        ctrl := Control{Message: "Hello from Go!"}
-        context := engine.Context()
-        context.SetVar("ctrl", &ctrl)
+	ctrl := control{}
+	context := engine.Context()
+	context.SetVar("ctrl", &ctrl)
 
-        win := component.CreateWindow(nil)
-        ctrl.Root = win.Root()
+	win := component.CreateWindow(nil)
+	ctrl.Root = win.Root()
 
-        win.Show()
-        win.Wait()
-        return nil
+	resp, err := http.Get("https://api.imgur.com/3/gallery/hot/viral/0.json")
+	defer resp.Body.Close()
+
+	var imgurResp response
+
+	if err := json.NewDecoder(resp.Body).Decode(&imgurResp); err != nil {
+		log.Println(err)
+	}
+
+	ctrl.K = 0
+	ctrl.Images = imgurResp.Data
+	ctrl.Source = ctrl.Images[ctrl.K].Link
+
+	win.Show()
+	win.Wait()
+	return nil
 }
 
-type Control struct {
-	Root    qml.Object
-	Message string
-}
-
-func (ctrl *Control) Hello() {
-        go func() {
-                if (ctrl.Message == "Hello from Go!") {
-                        ctrl.Message = "Hello from Go Again!"
-                } else {
-                        ctrl.Message = "Hello from Go!"
-                }
-                qml.Changed(ctrl, &ctrl.Message)
-        }()
+func (ctrl *control) Next() {
+	go func() {
+		ctrl.K++
+		ctrl.Source = ctrl.Images[ctrl.K].Link
+		log.Println(ctrl.Source)
+		qml.Changed(ctrl, &ctrl.Source)
+	}()
 }
